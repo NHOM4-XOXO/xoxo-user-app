573ec6f5b063bba23d14c85d6296936d147a8440
Uyentnm <uyen.truongnm@gmail.com>
2025-07-24 13:39:33 +0700

    update sidefriends

--- src/components/friends/SideFriends.jsx
+++ src/components/friends/SideFriends.jsx
@@ -28,34 +28,36 @@ export default function SidebarFriend() {
   const pathname = usePathname();
 
   return (
-    <aside className="w-72 sm:w-80 bg-white dark:bg-fb-dark-primary p-4 h-[calc(100vh-56px)] overflow-y-auto hidden lg:block">
-      <h2 className="text-xl font-semibold mb-4 px-2 text-black dark:text-white">
-        Bạn bè
-      </h2>
-      <ul className="space-y-1">
-        {items.map((item, idx) => {
-          const isActive = pathname === item.href;
-          return (
-            <li key={idx}>
-              <Link
-                href={item.href}
-                className={`flex items-center space-x-3 p-3 rounded-lg transition-colors
+    <aside className="w-72 sm:w-80 bg-white dark:bg-fb-dark-primary p-4  overflow-y-auto hidden lg:block">
+      <div className="aside-children fixed">
+        <h2 className="text-xl font-semibold mb-4 px-2 text-black dark:text-white">
+          Bạn bè
+        </h2>
+        <ul className="space-y-1">
+          {items.map((item, idx) => {
+            const isActive = pathname === item.href;
+            return (
+              <li key={idx}>
+                <Link
+                  href={item.href}
+                  className={`flex items-center space-x-3 p-3 rounded-lg transition-colors
                   ${
                     isActive
                       ? "bg-gray-200 dark:bg-gray-700 font-semibold"
                       : "hover:bg-gray-100 dark:hover:bg-gray-800"
                   }
                 `}
-              >
-                <span className="text-xl text-blue-600">{item.icon}</span>
-                <span className="text-sm text-black dark:text-white">
-                  {item.label}
-                </span>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+                >
+                  <span className="text-xl text-blue-600">{item.icon}</span>
+                  <span className="text-sm text-black dark:text-white">
+                    {item.label}
+                  </span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </aside>
   );
 }
